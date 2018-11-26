dbfeb005e3fa684865f2e2f69581059e65d48527
Kang Hyojun <iam.kanghyojun@gmail.com>
2018-11-27 01:18:05 +0900

    API 분리

--- mastermisi/wsgi.py
+++ mastermisi/wsgi.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+
+from .api import api
 from .web import web
 
 
 def create_wsgi_app() -> Flask:
     app = Flask(__name__)
+    app.register_blueprint(api)
     app.register_blueprint(web)
     return app
-
-
