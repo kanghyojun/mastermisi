32bccc6c703130150672a2230954b87bcbff8a0c
Ahn Kiwook <AiOO@users.noreply.github.com>
2018-11-28 06:23:34 +0900

    Implement signup (#9)
    * Implement signup
    
    * Add tests

--- tests/web_test.py
+++ tests/web_test.py
@@ -1,5 +1,7 @@
 from flask import url_for
 
+from mastermisi.entity import Customer
+
 
 def get_url(app, *args, **kwargs):
     with app.test_request_context():
@@ -34,3 +36,41 @@ def test_login_fail(fx_wsgi_app, fx_customer):
         assert resp.status_code == 302
         with c.session_transaction() as s:
             assert s['_flashes'][1][1] == '이름 혹은 암호가 잘못되었습니다.'
+
+
+def test_signup(fx_wsgi_app, fx_session):
+    url = get_url(fx_wsgi_app, 'mastermisi.web.signup')
+    assert not fx_session.query(Customer).filter_by(
+        name='hello',
+        passphrase=Customer.create_passphrase('world')
+    ).first()
+    with fx_wsgi_app.test_client() as c:
+        resp = c.post(url, data={
+            'name': 'hello',
+            'passphrase': 'world',
+        })
+        assert resp.status_code == 302
+        with c.session_transaction() as s:
+            assert s['_flashes'][0][1] == '가입이 완료되었습니다.'
+    assert fx_session.query(Customer).filter_by(
+        name='hello',
+        passphrase=Customer.create_passphrase('world')
+    ).one()
+
+
+def test_signup_fail(fx_wsgi_app, fx_customer):
+    url = get_url(fx_wsgi_app, 'mastermisi.web.signup')
+    with fx_wsgi_app.test_client() as c:
+        resp = c.post(url, data={
+            'name': 'hello',
+        })
+        assert resp.status_code == 302
+        with c.session_transaction() as s:
+            assert s['_flashes'][0][1] == '모든 값을 입력해주세요.'
+        resp = c.post(url, data={
+            'name': 'hello',
+            'passphrase': 'world',
+        })
+        assert resp.status_code == 302
+        with c.session_transaction() as s:
+            assert s['_flashes'][1][1] == '이미 존재하는 이름입니다.'
