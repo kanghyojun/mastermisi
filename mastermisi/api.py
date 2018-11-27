565a62dda0785b222d557b3fea464369c677c2e7
Kang Hyojun <iam.kanghyojun@gmail.com>
2018-11-27 17:51:21 +0900

    Additional

--- mastermisi/api.py
+++ mastermisi/api.py
@@ -1,23 +1,31 @@
-from flask import Blueprint, Response, jsonify
+from flask import Blueprint, Response, jsonify, request
 
 from .login import authorized
 
 __all__ = 'api',
-api = Blueprint(__name__, 'api')
+api = Blueprint(__name__, 'api', url_prefix='/api')
 
 
 @api.route('/auth/', methods=['POST'])
 def auth() -> Response:
     """브라우저 익스텐션용 로그인 API"""
-    return jsonify()
+    return jsonify(token='helloworld')
+
+
+@api.route('/passwords/', methods=['POST'])
+def get_passwords() -> Response:
+    """특정 웹사이트의 패스워드가 존재하는지 여부를 판단합니다."""
+    payload = request.get_json()
+    assert 'url' in payload
+    return jsonify({'id': 1})
 
 
 @api.route('/passwords/<int:id>/approvals/', methods=['POST'])
 @authorized
-def create_pending_approvals() -> Response:
+def create_pending_approvals(id: int) -> Response:
     """로그인을 위한 어프루브를 생성한다."""
-    return jsonify()
-
+    # 패스워드의 id가 아닌 approval의 id가 필요
+    return jsonify(id=123)
 
 
 @api.route('/pending-approvals/<int:id>/approved/', methods=['GET'])
@@ -30,9 +38,9 @@ def is_approved() -> Response:
 
 @api.route('/pending-approvals/<int:id>/', methods=['POST'])
 @authorized
-def approve() -> Response:
+def approve(id) -> Response:
     """로그인 어프루브할 것인지 말것인지 정하는 엔드포인트. 퀴즈가 맞지않다면
     로그인 실패처리하게 하도록해야함.
 
     """
-    return jsonify()
+    return jsonify(password='', id='')
