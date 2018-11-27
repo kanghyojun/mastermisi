6353f5587fb0d14c954d05235698e7183d6b2fdf
Ahn Kiwook <ahnkiwook@gmail.com>
2018-11-28 01:13:54 +0900

    Make migration works [WIP]

--- mastermisi/app.py
+++ mastermisi/app.py
@@ -1,5 +1,8 @@
+from settei.base import config_property
 from settei.presets.flask import WebConfiguration
 
 class App(WebConfiguration):
-
-    pass
+    """서버 애플리케이션입니다."""
+    database_url = config_property(
+        'database.url', str, "데이터베이스 연결용 문자열"
+    )
--- mastermisi/orm.py
+++ mastermisi/orm.py
@@ -1,5 +1,8 @@
+from typing import Optional, Union
+
 from flask import current_app, has_request_context
 from alembic.config import Config
+from sqlalchemy.engine import Engine
 from sqlalchemy.ext.declarative import as_declarative
 from sqlalchemy.orm import sessionmaker
 from ormeasy.sqlalchemy import repr_entity
@@ -36,16 +39,9 @@ def get_alembic_config(
     app: Optional['mastermisi.app.App'] = None,
     engine: Union[None, str, Engine] = None
 ) -> Config:
-    """Create alembic configuration.
-
-    :param app:
-    :param engine:
-    :return: The alembic configuration.
-    :rtype: :class:`alembic.config.Config`
-
-    """
+    """alembic 설정을 만듭니다."""
     if not (app or engine):
-        raise ValueError('One of `app` or `engine` MUST be given.')
+        raise ValueError('`app` 또는 `engine` 값이 있어야 합니다.')
     if app:
         database_url = app.database_url
     elif isinstance(engine, Engine):
@@ -53,11 +49,13 @@ def get_alembic_config(
     elif isinstance(engine, str):
         database_url = engine
     else:
-        raise ValueError('`engine` MUST be instance of `str` or '
+        raise ValueError('`engine` 값은 `str` 이거나 '
                          '`sqlalchemy.engine.Engine`. '
-                         'not {!r}.'.format(engine))
+                         '이어야 합니다. {!r} 입력됨.'.format(engine))
     cfg = Config()
-    cfg.set_main_option('script_location', 'mastermisi:migrations')
+    # TODO: mastermisi:migraions로 하면 설정이 없다 그러고
+    #       mastermisi/migrations로 하면 디렉터리를 잘못 잡음
+    cfg.set_main_option('script_location', 'mastermisi/migrations')
     cfg.set_main_option('sqlalchemy.url', database_url)
     cfg.set_main_option('url', database_url)
     return cfg
