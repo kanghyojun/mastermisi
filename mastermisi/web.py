f3ed74934858700692ef8c14c47058d608bfb2c9
Ahn Kiwook <ahnkiwook@gmail.com>
2018-11-28 04:48:33 +0900

    Implement login

--- mastermisi/web.py
+++ mastermisi/web.py
@@ -1,10 +1,13 @@
 from flask import (Blueprint, Response, flash, redirect, render_template,
                    request, url_for)
+from sqlalchemy.orm.exc import NoResultFound
 from wtforms.fields import PasswordField, StringField
 from wtforms.form import Form
 from wtforms.validators import input_required
 
+from .entity import Customer
 from .login import login_required
+from .orm import session
 
 __all__ = 'web',
 web: Blueprint = Blueprint(__name__, 'web', template_folder='./templates')
@@ -19,7 +22,7 @@ class SignForm(Form):
 
     name = StringField(u'이름', validators=[input_required()])
 
-    passphrase = PasswordField(u'비밀 번호', validators=[input_required()])
+    passphrase = PasswordField(u'암호', validators=[input_required()])
 
 
 @web.route('/', methods=['GET'])
@@ -40,9 +43,22 @@ def login() -> Response:
     """로그인을 시키고 패스워드 페이지를 볼 수 있게 인증함."""
     form = SignForm('로그인', url_for('.login'), request.form)
     if not form.validate():
-        flash('에러')
-        return redirect(url_for('.passwords'))
-    return redirect(url_for('.passwords'))
+        flash('모든 값을 입력해주세요.')
+        return redirect(url_for('.login_form'))
+    passphrase = Customer.create_passphrase(form.passphrase.data)
+    try:
+        customer = session.query(
+            Customer
+        ).filter_by(
+            name=form.name.data,
+            passphrase=passphrase
+        ).one()
+    except NoResultFound:
+        flash('이름 혹은 암호가 잘못되었습니다.')
+        return redirect(url_for('.login_form'))
+    else:
+        flash(f'{customer.name}님 안녕하세요!')
+        return redirect(url_for('.accounts'))
 
 
 @web.route('/passcode/', methods=['GET'])
@@ -51,11 +67,11 @@ def get_passcode() -> Response:
     return '1234'
 
 
-@web.route('/passwords/', methods=['GET'])
+@web.route('/accounts/', methods=['GET'])
 @login_required
-def passwords() -> Response:
+def accounts() -> Response:
     """패스워드 리스트를 볼 수 있고, 패스워드 생성도 할 수 있어야함."""
-    return 'password list & create'
+    return render_template('accounts.html')
 
 
 @web.route('/passwords/', methods=['POST'])
